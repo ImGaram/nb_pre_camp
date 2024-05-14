d480bb58fbc35e1c96edc38a206ada1eb068adc1
ImGaram <gim78014@gmail.com>
2024-05-14 16:29:20 +0900

    :lipstick: :: mbti 앱 메인 화면(3주차 1강)

--- mbtiTest/app/src/main/java/com/example/mbtitest/TestActivity.kt
+++ mbtiTest/app/src/main/java/com/example/mbtitest/TestActivity.kt
@@ -1,28 +1,20 @@
 package com.example.mbtitest
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_test)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnTest = findViewById<ImageView>(R.id.iv_start)
-        btnTest.setOnClickListener {
-            val intent = Intent(this, TestActivity::class.java)
-            startActivity(intent)
         }
     }
 }
