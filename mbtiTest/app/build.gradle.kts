f0bf2924445a6c77485e3bbde1f89fd11bf1d3da
ImGaram <gim78014@gmail.com>
2024-05-14 17:05:48 +0900

    :lipstick: :: mbti 앱 질문지 화면 UI(3주차 1강)

--- mbtiTest/app/build.gradle.kts
+++ mbtiTest/app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.viewpager2)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- mbtiTest/app/src/main/java/com/example/mbtitest/TestActivity.kt
+++ mbtiTest/app/src/main/java/com/example/mbtitest/TestActivity.kt
@@ -1,12 +1,16 @@
 package com.example.mbtitest
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
 
 class TestActivity : AppCompatActivity() {
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,29 @@ class TestActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        viewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = ViewPagerAdapter(this)
+        viewPager.isUserInputEnabled = false
+    }
+
+    fun moveToNextQuestion() {
+        if (viewPager.currentItem == 3) {
+
+        } else {
+            val nextItem = viewPager.currentItem + 1
+            if (nextItem < (viewPager.adapter?.itemCount ?: 0)) {
+                viewPager.setCurrentItem(nextItem, true)
+            }
+        }
+    }
+}
+
+class QuestionnaireResults() {
+    val results = mutableListOf<Int>()
+
+    fun addResponses(response: List<Int>) {
+        val mostFrequent = response.groupingBy { it }.eachCount().maxByOrNull { it.value }?.key
+        mostFrequent?.let { results.add(it) }
     }
 }
