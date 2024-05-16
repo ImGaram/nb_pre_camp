fc39b48f69a586d1c45290f816e65c7545d5ba62
ImGaram <gim78014@gmail.com>
2024-05-16 16:42:05 +0900

    :lipstick: :: mbti 앱 질문지 기능(3주차 3강)

--- mbtiTest/app/src/main/java/com/example/mbtitest/QuestionFragment.kt
+++ mbtiTest/app/src/main/java/com/example/mbtitest/QuestionFragment.kt
@@ -5,12 +5,63 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
 
 class QuestionFragment : Fragment() {
+    private var questionType = 0
+    private val questionTitle = listOf(
+        R.string.question1_title,
+        R.string.question2_title,
+        R.string.question3_title,
+        R.string.question4_title,
+    )
+    private val questionTexts = listOf(
+        listOf(R.string.question1_1, R.string.question1_2, R.string.question1_3),
+        listOf(R.string.question2_1, R.string.question2_2, R.string.question2_3),
+        listOf(R.string.question3_1, R.string.question3_2, R.string.question3_3),
+        listOf(R.string.question4_1, R.string.question4_2, R.string.question4_3),
+    )
+    private val questionAnswers = listOf(
+        listOf(
+            listOf(R.string.question1_1_answer1, R.string.question1_1_answer2),
+            listOf(R.string.question1_2_answer1, R.string.question1_2_answer2),
+            listOf(R.string.question1_3_answer1, R.string.question1_3_answer2),
+        ),
+        listOf(
+            listOf(R.string.question2_1_answer1, R.string.question2_1_answer2),
+            listOf(R.string.question2_2_answer1, R.string.question2_2_answer2),
+            listOf(R.string.question2_3_answer1, R.string.question2_3_answer2),
+        ),
+        listOf(
+            listOf(R.string.question3_1_answer1, R.string.question3_1_answer2),
+            listOf(R.string.question3_2_answer1, R.string.question3_2_answer2),
+            listOf(R.string.question3_3_answer1, R.string.question3_3_answer2),
+        ),
+        listOf(
+            listOf(R.string.question4_1_answer1, R.string.question4_1_answer2),
+            listOf(R.string.question4_2_answer1, R.string.question4_2_answer2),
+            listOf(R.string.question4_3_answer1, R.string.question4_3_answer2),
+        ),
+    )
+
+    companion object {
+        private const val ARG_QUESTION_TYPE = "questionType"
+
+        fun newInstance(questionType: Int): QuestionFragment {
+            val fragment = QuestionFragment()
+            val args = Bundle()
+            args.putInt(ARG_QUESTION_TYPE, questionType)
+            fragment.arguments = args
+
+            return fragment
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        arguments?.let {
+            questionType = it.getInt(ARG_QUESTION_TYPE)
+        }
     }
 
     override fun onCreateView(
