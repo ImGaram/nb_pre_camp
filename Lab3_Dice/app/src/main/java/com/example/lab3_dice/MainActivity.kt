703b1ec440ae3d53c5973942989d8997496f084f
ImGaram <gim78014@gmail.com>
2024-06-18 16:40:23 +0900

    :tada: :: 안드로이드 앱 개발 입문 2주차 랜덤 주사위 프로젝트 실습

--- Lab3_Dice/app/src/main/java/com/example/lab3_dice/MainActivity.kt
+++ Lab3_Dice/app/src/main/java/com/example/lab3_dice/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.lab3_dice
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val tvNum = findViewById<TextView>(R.id.tv_num)
+        val button = findViewById<Button>(R.id.btn_roll)
+
+        button.setOnClickListener {
+            val random = Random
+            val num = random.nextInt(6) +1
+
+            tvNum.text = num.toString()
+
+            Log.d("MainActivity", "num = $num")
         }
     }
 }
